97d4b6a3be4b6008afa64d32ffae565a36068888
LMellaVillanueva <mellalucas.v@gmail.com>
2024-06-12 17:15:33 -0400

    animaciones pocas

--- src/components/burguerMenu/BurguerMenu.jsx
+++ src/components/burguerMenu/BurguerMenu.jsx
@@ -4,10 +4,20 @@ import styles from './burguerMenu.module.css';
 const BurguerMenu = ({ handleNavigation }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [animation, setAnimation] = useState(false);
+  const [landing, setLanding] = useState(false);
+  const [proyectos, setProyectos] = useState(false);
+  const [aboutMe, setAboutMe] = useState(false);
+  const [tech, setTech] = useState(false);
+  const [contact, setContact] = useState(false);
 
   useEffect(() => {
-    if(isOpen){
+    if (isOpen) {
       setAnimation(true);
+      setLanding(true);
+      setProyectos(true);
+      setAboutMe(true);
+      setTech(true);
+      setContact(true);
     }
   }, [isOpen])
 
@@ -20,14 +30,14 @@ const BurguerMenu = ({ handleNavigation }) => {
       </div>
       {isOpen && (
         <div className={styles.menuContent}>
-        <button className={`${styles.closeButton} ${animation ? styles.anim : ''} `} onClick={() => setIsOpen(false)}>
-          X
-        </button>
-          <a href="#landing" onClick={(event) => { handleNavigation(event, 'landing'); setIsOpen(false) }}>Inicio</a>
-          <a href="#proyectos" onClick={(event) => { handleNavigation(event, 'proyectos'); setIsOpen(false) }}>Proyectos</a>
-          <a href="#aboutMe" onClick={(event) => { handleNavigation(event, 'aboutMe'); setIsOpen(false) }}>Sobre Mí</a>
-          <a href="#technologies" onClick={(event) => { handleNavigation(event, 'technologies'); setIsOpen(false) }}>Tecnologías</a>
-          <a href="#contacto" onClick={(event) => { handleNavigation(event, 'contacto'); setIsOpen(false) }}>Contacto</a>
+          <button className={`${styles.closeButton} ${animation ? styles.anim : ''} `} onClick={() => setIsOpen(false)}>
+            X
+          </button>
+          <a href="#landing" className={`${landing ? styles.landing : ''}`} onClick={(event) => { handleNavigation(event, 'landing'); setIsOpen(false) }}>Inicio</a>
+          <a href="#proyectos" className={`${proyectos ? styles.proyectos : ''}`} onClick={(event) => { handleNavigation(event, 'proyectos'); setIsOpen(false) }}>Proyectos</a>
+          <a href="#aboutMe" className={`${aboutMe ? styles.aboutMe : ''}`} onClick={(event) => { handleNavigation(event, 'aboutMe'); setIsOpen(false) }}>Sobre Mí</a>
+          <a href="#technologies" className={`${tech ? styles.tech : ''}`} onClick={(event) => { handleNavigation(event, 'technologies'); setIsOpen(false) }}>Tecnologías</a>
+          <a href="#contacto" className={`${contact ? styles.contact : ''}`} onClick={(event) => { handleNavigation(event, 'contacto'); setIsOpen(false) }}>Contacto</a>
         </div>
       )}
     </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,12 +4,19 @@ import gh from '../../assets/gh.png';
 import linkedIN from '../../assets/linkedIN.png';
 import wsp from '../../assets/wsp.png';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import { useEffect, useState } from 'react';
 
 export const Header = () => {
+const [anim, setAnim] = useState(false);
+
+    useEffect(() => {
+        setAnim(true);
+    }, [])
+
     return (
         <>
             <div className={`flex flex-col lg:flex-row items-center lg:justify-center gap-7 lg:gap-4 h-fit lg:h-2/4 w-screen mb-8 lg:mb-0 mt-20 lg:mt-44 bg-gradient-to-b from-black via-purple-950 ${styles.allContainer}`}>
-                <div className={`z-10 flex flex-col lg:flex-row p-3 lg:p-8 justify-center items-center lg:gap-20 w-10/12 lg:w-7/12 border rounded-sm border-purple-500 font-sans ${styles.shadowContainer} ${styles.presentacion}`}>
+                <div className={`z-10 flex flex-col lg:flex-row p-3 lg:p-8 justify-center items-center lg:gap-20 w-10/12 lg:w-7/12 border rounded-sm border-purple-500 font-sans ${styles.shadowContainer} ${anim ? styles.anim : ''} ${anim ? styles.border : ''}`}>
                     <div className={`flex flex-col lg:text-start ${styles.text}`}>
                         <h2 className={`lg:text-xl text-white ${styles.texto}`}>Eyyy! Soy</h2>
                         <h1 className={`mb-3 mt-3 text-3xl text-white ${styles.texto}`}>YoeLukitas,</h1>
@@ -17,7 +24,7 @@ export const Header = () => {
                     </div>
                     <img src={selfie} alt="Retrato" className={`w-44 lg:w-56`} />
                 </div>
-                <div className={`flex lg:flex-col gap-3 w-fit ${styles.logos} ${styles.logosContainer}`}>
+                <div className={`flex lg:flex-col gap-3 w-fit ${styles.logos} ${anim ? styles.anim : ''}`}>
                     <a className={styles.logos} href="https://wa.me/981314078">
                         <img src={wsp} alt="Wsp" className='w-10 lg:w-11' />
                     </a>
@@ -29,9 +36,9 @@ export const Header = () => {
                     </a>
                 </div>
             </div>
-            <div className={`flex justify-center items-center gap-5 lg:gap-10 font-sans ${styles.cv}`}>
-                <a href="/LucasMellaCV.pdf" className={`p-3 rounded-sm bg-gradient-to-b from-black via-gray-900 to-purple-900 font-sans hover:scale-105 transition-all duration-150 border border-purple-500 hover:text-purple-300 ${styles.shadow}`}>Visualiza mi CV!</a>
-                <a href="/LucasMellaCV.pdf" download className={`hover:scale-105 transition-all duration-150 hover:text-purple-400 p-3 border border-purple-500 rounded-sm bg-gradient-to-b from-black via-gray-900 to-purple-900 ${styles.shadow}`}><FileDownloadIcon /></a>
+            <div className={`flex justify-center items-center gap-5 lg:gap-10 font-sans ${styles.cv} ${anim ? styles.anim : ''} ${anim ? styles.shadow : ''}`}>
+                <a href="/LucasMellaCV.pdf" className={`p-3 rounded-sm bg-gradient-to-b from-black via-gray-900 to-purple-900 font-sans hover:scale-105 transition-all duration-150 border border-purple-500 hover:text-purple-300`}>Visualiza mi CV!</a>
+                <a href="/LucasMellaCV.pdf" download className={`hover:scale-105 transition-all duration-150 hover:text-purple-400 p-3 border border-purple-500 rounded-sm bg-gradient-to-b from-black via-gray-900 to-purple-900`}><FileDownloadIcon /></a>
             </div>
         </>
     );
