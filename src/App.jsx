3a6ca4d441a5b18607568431c8e5bbb1a310cce6
LMellaVillanueva <mellalucas.v@gmail.com>
2024-12-06 21:06:37 -0300

    landing eliminada

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,7 @@ import Footer from './components/footer/Footer';
 import BurguerMenu from './components/burguerMenu/BurguerMenu';
 
 function App() {
-  const [time, setTime] = useState(false);
   const [show, setShow] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setTime(true);
-    }, 4500);
-  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -51,31 +44,25 @@ function App() {
   return (
     <>
       <div className='flex flex-col items-center min-h-screen w-screen overflow-y-auto overflow-x-hidden'>
-        {time && (
           <>
             <nav id='navBar' className={`relative -mb-10 z-20 top-5 w-full text-white flex justify-center font-sans text-xl shadow`}>
               <div className='flex space-x-10 p-4'>
-                <a href="#landing" onClick={(event) => handleNavigation(event, 'landing')} className={` ${time ? 'inicio' : ''}`}>Inicio</a>
-                <a href="#proyectos" onClick={(event) => handleNavigation(event, 'proyectos')} className={` ${time ? 'proyectos' : ''}`}>Proyectos</a>
-                <a href="#aboutMe" onClick={(event) => handleNavigation(event, 'aboutMe')} className={` ${time ? 'about' : ''}`}>Sobre Mí</a>
-                <a href="#technologies" onClick={(event) => handleNavigation(event, 'technologies')} className={` ${time ? 'tech' : ''}`}>Tecnologías</a>
-                <a href="#contacto" onClick={(event) => handleNavigation(event, 'contacto')} className={` ${time ? 'contact' : ''}`}>Contacto</a>
+                <a href="#landing" onClick={(event) => handleNavigation(event, 'landing')} className='inicio'>Inicio</a>
+                <a href="#proyectos" onClick={(event) => handleNavigation(event, 'proyectos')} className='proyectos'>Proyectos</a>
+                <a href="#aboutMe" onClick={(event) => handleNavigation(event, 'aboutMe')} className='about'>Sobre Mí</a>
+                <a href="#technologies" onClick={(event) => handleNavigation(event, 'technologies')} className='tech'>Tecnologías</a>
+                <a href="#contacto" onClick={(event) => handleNavigation(event, 'contacto')} className='contact'>Contacto</a>
               </div>
             </nav>
             <div id='burguerMenu'>
               <BurguerMenu handleNavigation={handleNavigation} />
             </div>
           </>
-        )}
         <Landing />
-        {time && (
-          <>
             <TimeLine />
             <AboutMe />
             <Technologies />
             <Footer />
-          </>
-        )}
         <div className={`upButton ${show ? 'show' : 'hide'}`}>
           <i onClick={scrollUp}><ExpandLessIcon /></i>
         </div>
--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,50 +1,14 @@
-import { useState } from 'react';
-import styles from './landing.module.css';
-import { useEffect } from 'react';
 import Header from '../header/Header';
 
 export const Landing = () => {
-    const [fadeIn, setFadeIn] = useState(false);
-    const [fadeOut, setFadeOut] = useState(false);
-    const [bg, setBg] = useState(false);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setFadeIn(true)
-        }, 1000);
-
-        setFadeIn(false)
-
-        if (!fadeIn) {
-            setTimeout(() => {
-                setFadeOut(true)
-            }, 3500);
-        }
-
-        setFadeOut(false)
-    }, [])
-
-    useEffect(() => {
-        setTimeout(() => {
-            setBg(true)
-        }, 4500);
-    }, [])
 
     return (
-        <>
-            <div id='landing' className='relative h-screen w-full flex items-center justify-center'>
-                {!bg ? (
-                    <div className='absolute inset-0 bg-black'></div>
-                ) : (
-                    <div className={`absolute inset-0 z-10 ${bg ? styles.bg : ''}`}><Header />
-                    </div>
-                )}
-                <div className={`relative inset-0 ${styles.container}`}>
-                    <h1 className={`${styles.text} ${fadeIn ? styles.fadeIn : ''} ${fadeOut ? styles.fadeOut : ''} text-4xl text-center text-white p-4 font-sans`}>¡Te doy la bienvenida!</h1>
+        <div id="landing" className="relative h-screen w-full flex items-center justify-center">
+                <div className={`absolute inset-0 z-10`}>
+                    <Header />
                 </div>
-            </div>
-        </>
-    )
-}
+        </div>
+    );
+};
 
 export default Landing;
